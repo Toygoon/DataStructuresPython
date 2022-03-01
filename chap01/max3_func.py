ba417bc33e71f0aac65bc15f0444ffe7bacddf96
Toygoon <toygoon@gmail.com>
2022-03-01 15:58:23 +0900

    Ex1-2

---
+++ chap01/max3_func.py
@@ -0,0 +1,21 @@
+# Find out max integer
+
+def max3(a, b, c):
+    max = a
+    if b > max:
+        max = b
+    if c > max:
+        max = c
+    
+    return max
+
+print(f'max3(3, 2, 1) : {max3(3, 2, 1)}')
+print(f'max3(3, 2, 2) : {max3(3, 2, 2)}')
+print(f'max3(3, 1, 2) : {max3(3, 1, 2)}')
+print(f'max3(3, 2, 3) : {max3(3, 2, 3)}')
+print(f'max3(2, 1, 3) : {max3(2, 1, 3)}')
+print(f'max3(3, 3, 2) : {max3(3, 3, 2)}')
+print(f'max3(3, 3, 3) : {max3(3, 3, 3)}')
+print(f'max3(2, 2, 3) : {max3(2, 2, 3)}')
+print(f'max3(2, 3, 1) : {max3(2, 3, 1)}')
+print(f'max3(2, 3, 2) : {max3(2, 3, 2)}')
